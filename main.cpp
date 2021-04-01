034c2c94e5ba9b7f8072261ecfa7b4cbb7dcea4d
Yuliya Solovieva <mshairin@mail.ru>
2021-04-01 15:36:02 +0900

    код питон

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
-cout<<"gei"
-cout<<"syus"
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+void print_gei(){ 
+cout<<"Гоголева Эра Иннокентьевна"
+
+}
+
+void print_syus(){
+cout<<"Соловьева Юлия Сергеевна"
+}
+
 #файл был изменен
